2cc30cb31e0bec0173ae48d4a6c67a7a8eb2d3e5
Kaivalya Thombare <itskv5107.kt@gmail.com>
2026-08-15 10:55:53 +0530

    Update ResumePreview.tsx

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -3,36 +3,17 @@ import type { ResumeData } from '@/types';
 import ModernTemplate from '@/resume/MordenTemplate';
 import ClassicTemplate from '@/resume/ClassicTemplate';
 import MinimalTemplate from '@/resume/MinimalTemplate';
-
-type Props = { data: ResumeData };import { forwardRef } from 'react';
-import type { ResumeData } from '@/types';
-import ModernTemplate from '@/resume/MordenTemplate';
-import ClassicTemplate from '@/resume/ClassicTemplate';
-import MinimalTemplate from '@/resume/MinimalTemplate';
 import CreativeTemplate from '@/resume/CreativeTemplate';
 
 type Props = { data: ResumeData };
 
 const ResumePreview = forwardRef<HTMLDivElement, Props>(({ data }, ref) => {
   return (
-    <div ref={ref} className="h-full w-full">
+    <div ref={ref} id="resume-preview" className="h-full w-full bg-white">
       {data.template === 'modern'   && <ModernTemplate   data={data} />}
       {data.template === 'classic'  && <ClassicTemplate  data={data} />}
       {data.template === 'minimal'  && <MinimalTemplate  data={data} />}
       {data.template === 'creative' && <CreativeTemplate data={data} />}
-    </div>
-  );
-});
-
-ResumePreview.displayName = 'ResumePreview';
-export default ResumePreview;
-
-const ResumePreview = forwardRef<HTMLDivElement, Props>(({ data }, ref) => {
-  return (
-    <div ref={ref} id="resume-preview" className="h-full w-full bg-white">
-      {data.template === 'modern' && <ModernTemplate data={data} />}
-      {data.template === 'classic' && <ClassicTemplate data={data} />}
-      {data.template === 'minimal' && <MinimalTemplate data={data} />}
     </div>
   );
 });
